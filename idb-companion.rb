60493610ba0c5b3b18cf5843567afbf7ff254052
wooship <31851430+wooship@users.noreply.github.com>
2021-04-22 11:37:08 +0800

    Update idb-companion.rb

--- idb-companion.rb
+++ idb-companion.rb
@@ -7,7 +7,7 @@
 class IdbCompanion < Formula
   desc "A Powerful Command Line for automating iOS Simulators"
   homepage "https://github.com/facebook/idb/README.md"
-  url "https://github.com/facebook/idb/archive/v1.1.2.tar.gz"
+  url "https://github.com/facebook/idb/archive/v1.0.14.tar.gz"
   sha256 "fe52b96f22be6aa6161c1a0e09c04f34f9b5207c49cc0eb16b3e0b3c03744e42"
   head "https://github.com/facebook/idb.git"
 
@@ -15,7 +15,7 @@ class IdbCompanion < Formula
   depends_on "grpc" => "1.29.1"
 
   bottle do
-    root_url "https://github.com/facebook/idb/releases/download/v1.1.2"
+    root_url "https://github.com/facebook/idb/releases/download/v1.0.14"
     sha256 cellar: :any, big_sur: "50d8dfdda62d984d50f6f095674725861224b70470bf2bc65fdcdd540b1533bb"
   end
 
